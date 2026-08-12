4c5e4ec5c1861af8fd235080dc8c709458b4aa96
Shefali Sharma <shefalisharmaa4455@gmail.com>
2026-08-12 20:05:58 +0530

    Add files via upload

---
+++ 3_Datatypes/datatypes.java
@@ -0,0 +1,23 @@
+public class datatypes {
+    public static void main(String[] args) {
+    // datatypes are the declaration that defins the type, size and rang of the value that would be stored in a variable 
+
+    // primitive datatypes
+    // int
+    byte by= 12;
+    int num1 = 9;
+    short sh = 5432;
+    long l = 4328l;
+
+    // float
+    float f = 8.9f;
+    double d = 4.5;
+
+    // boolean
+    boolean b = true;
+    boolean value = false;
+
+    // char
+    char ch = 'a';
+    }
+}
