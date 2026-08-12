566535bf75e43ce99b7b047b6a04605d28bb0a74
Shefali Sharma <shefalisharmaa4455@gmail.com>
2026-08-12 20:05:28 +0530

    Add files via upload

---
+++ 2_Variables/variable.java
@@ -0,0 +1,14 @@
+public class variable {
+    public static void main(String[] args) {
+        // variable - names of the temp blocks in the memory where we can store our values
+        //print () - method donotr create any new line between the outputs
+        //println () - method create any new line between the outputs
+        // java is a strongly typed language
+        int num1 = 2; // datatypes int 
+        int num2 = 3;
+        System.out.println(num1 + num2);//5
+
+
+    }
+    
+}
